8fbd3b3628508f47e1e639d901d9c0b2e7581230
Arthur Gaspar <160799933+Artgspr@users.noreply.github.com>
2025-04-07 20:54:50 -0300

    07_04
    versão final do primeiro bin

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,41 @@
 import { Text, View, StyleSheet} from "react-native";
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
+import Button from '@/app/components/button';
 import ImageViewer from '@/app/components/imageViewer';
 
-const PlaceholderImage = require('@/assets/images/background-image.png')
+const PlaceholderImage = require('@/assets/images/peixoto2.jpg')
+
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
   return (
-    <View style={styles.container}
-    >
+    <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
+        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
-      <h1 style={styles.h1}>JOGO DO PEIXOTO</h1>
-      <h2 style={styles.h2}>PAGANDO MUITO!</h2>
+      <View style={styles.footerContainer}>
+        <h1 style={styles.h1}>JOGO DO PEIXOTO</h1>
+        <h2 style={styles.h2}>PAGANDO MUITO!</h2>
+        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync}/>
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
@@ -40,14 +63,20 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
   },
 
-  imageContainer:{
-    flex: 1,
-  },
-
   image: {
     width: 320,
     height: 440,
     borderRadius: 18,
-  }
+  },
 
-})
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+    marginBottom: 200,
+    },
+  });
